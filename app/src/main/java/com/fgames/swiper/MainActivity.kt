abec209b533500b3e789846a13566b903f58ec87
Andrey <nullnullru@gmail.com>
2020-05-11 13:32:24 +0300

    Create project commit

---
+++ app/src/main/java/com/fgames/swiper/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fgames.swiper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
